0a876ca7aee600db7ffff4a1178495c0a2be2314
Björn Guth <b.guth@netcon-consulting.com>
2022-02-12 15:59:59 +0100

    Git create a first version of an CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(long, short, parse(from_os_str))]
+    config: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:#?}", args);
 }
